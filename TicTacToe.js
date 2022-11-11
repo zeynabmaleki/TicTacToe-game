e26d6fd83d01daeafcc4056f8d013bd84c4f855d
zizimrad <73473371+zeynabmaleki@users.noreply.github.com>
2022-11-11 13:00:25 +0330

    making files

---
+++ TicTacToe.js
@@ -0,0 +1,20 @@
+let gameDiv = document.getElementsByClassName('game');
+
+let xButton = document.getElementById('x')
+let oButton = document.getElementById('o')
+
+
+xButton.addEventListener('click', choseX)
+oButton.addEventListener('click', choseO)
+
+function choseX() {
+
+}
+
+function choseO() {
+
+}
+
+if (choseX || choseO) {
+    console.log('workingggg')
+}
