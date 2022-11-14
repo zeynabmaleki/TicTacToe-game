382ff52f31ad0a2f53c6da1e3f766753d93620ae
zizimrad <73473371+zeynabmaleki@users.noreply.github.com>
2022-11-14 13:35:17 +0330

    theres sth wrong

--- TicTacToe.js
+++ TicTacToe.js
@@ -1,20 +1,36 @@
-let gameDiv = document.getElementsByClassName('game');
+let gameDiv = document.getElementById('game');
+let chooseDiv = document.getElementById('choose');
 
 let xButton = document.getElementById('x')
 let oButton = document.getElementById('o')
 
-
 xButton.addEventListener('click', choseX)
 oButton.addEventListener('click', choseO)
 
+// ======================
+// player chosed X button
+// ======================
 function choseX() {
+    chooseDiv.innerHTML = "";
 
-}
 
+    const none = 0
+    const x = 1
+    const o = 2
+
+    // starting the game
+    cell[i].addEventListener('click', clickedOnDiv)
+    
+    function clickedOnDiv() {
+
+    }
+};
+
+// ======================
+// player chosed O button
+// ======================
 function choseO() {
+    chooseDiv.innerHTML = "";
 
-}
 
-if (choseX || choseO) {
-    console.log('workingggg')
 }
